a99ef90abaa57b1b2a0dd608b4894f51f4dc0814
Anass Klaikel <aklaikel@e3r1p6.1337.ma>
2022-08-09 19:06:40 +0100

    run cmd in execution

---
+++ execution.c
@@ -0,0 +1,39 @@
+#include "sh.h"
+#include <stdio.h>
+#include <unistd.h>
+
+void	executor(t_tree *tree) {
+	int fd[2];
+	int pids[2];
+	if (!tree){
+		return ;
+	}
+	if (tree->type == CMD) {
+		// if builtin
+		//	call functions
+
+		int pid;
+		pid = fork();
+		if (pid == 0) {
+			// ls -la makefile
+			execvp((tree->cmd)[0], tree->cmd);
+			fprintf(stderr, "command not found!\n");
+			exit(1);
+		}
+
+	}
+	else if (tree->type == PIPE) {
+		return ;
+	// 	pipe(fd);
+	// 	pids[0] = fork();
+	// 	pids[1] = fork();
+	// 	if (pids[0] == 0) {
+	// 		executor(tree)
+	// 		exit(0);
+	// 	}
+	// 	if (pids[1] == 0) {
+			
+	// 		exit(0);
+	// 	}
+	// }
+}
