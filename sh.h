ce33c4205eff1b22dc55e7156a9d131906a8cf97
Oussama Moussaoui <omoussaoui1337@gmail.com>
2022-08-09 18:39:23 +0100

    Update header

--- sh.h
+++ sh.h
@@ -5,5 +5,24 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <stdbool.h>
+
+#define MAXSIZE 1024
+#define PIPE 1
+#define CMD 2
+
+typedef struct s_tree
+{
+	int type;
+	char **cmd;
+	struct s_tree *left;
+	struct s_tree *right;
+} t_tree;
+
+
+char	**ft_split(char const *s, char c);
+t_tree  *new_node(int type, char **cmd, t_tree *left, t_tree *right);
+t_tree  *parser(char **list);
+void    executor(t_tree *tree);
 
 #endif
