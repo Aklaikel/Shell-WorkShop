74318cbb39fdd9a8311f5adf15b2cdefb0607a74
Oussama Moussaoui <omoussaoui1337@gmail.com>
2022-08-09 19:21:42 +0100

    Update sh.c

--- sh.c
+++ sh.c
@@ -1,6 +1,52 @@
 #include "sh.h"
 
-int main()
+void prompt(char *cmdline)
 {
-	return 0;
+	printf("minishell> ");
+	fflush(stdout);
+	int nbytes = read(0, cmdline, MAXSIZE);
+	if (nbytes == -1)
+		exit(1);
+	cmdline[nbytes] = '\0';
+}
+
+int main(int ac, char **av, char **env)
+{
+	char   cmdline[MAXSIZE];
+	char   **list = NULL;
+	t_tree *tree = NULL;
+
+	while (true)
+	{
+		prompt(cmdline);
+
+		list = ft_split(cmdline, ' ');
+		if (!list)
+			exit(1);
+
+		tree = parser(list);
+		if (!tree)
+			return 2;
+
+		executor(tree);
+
+		// free memory: list, tree...
+		free_list(list);
+		free_tree(tree);
+	}
+}
+
+void free_tree(t_tree *tree) {
+	if (!tree)
+		return ;
+	free_tree(tree->left);
+	free_tree(tree->right);
+	free(tree);
+	return ;
+}
+
+void free_list(char **list) {
+	for (int i = 0; list[i]; i++)
+		free(list[i]);
+	free(list);
 }
